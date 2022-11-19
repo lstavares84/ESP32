8c80510971cb7148857b3812c03ecb39224c475b
lstavares84 <61010791+lstavares84@users.noreply.github.com>
2022-11-19 16:27:04 -0300

    Update led.blink.py

--- led.blink.py
+++ led.blink.py
@@ -6,11 +6,12 @@ import time #for sleep() function
 
 rest_time=0.5 #time of blinking
 
-led1 = machine.Pin(21, machine.Pin.OUT)
-led1.off #When the microcontroller is turned on or restarted, this guarantees that the LED will be in the initial operating stage.
+led1 = machine.Pin(21, machine.Pin.OUT) #It was used GPIO 21 to run this test.
+
+led1.off() #When the microcontroller is turned on or restarted, this guarantees that the LED will be in the initial operating stage.
 
 while True:
   led1.on()
-  sleep(rest_time)
+  time.sleep(rest_time)
   led1.off()
-  sleep(rest_time)
+  time.sleep(rest_time)
